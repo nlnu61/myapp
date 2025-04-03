f3aa823b6a49de6318ef4cb4ab5b5e5e85b3cdcb
navdeepkaur2272005 <navdeepkaur2272005@gmail.com>
2025-04-03 20:50:49 +0000

    Update

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/firebase_options.dart';
+import 'package:myapp/screens/home_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  //Initialize flutter bindings
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //Initialized Firebase with platforms default options
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomePage());
+  }
+}
